03b2b87af84543a87179ab98d8eb5a9e1adff056
Vasileios Millios <billmillios1@gmail.com>
2025-05-29 22:53:53 +0300

    delete old files

--- app/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-session_unset();    // Clear all session variables
-session_destroy();  // Destroy the session
-header("Location: index.php");
-exit;
